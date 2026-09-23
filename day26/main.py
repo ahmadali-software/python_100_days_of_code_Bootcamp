ec4c652615bd3524fe5b9830aac318388c6dc9e6
unknown <ahmadaliasdf15@gmail.com>
2026-09-23 12:36:48 -0500

    added error handling to day 26 project completing day 30tasks

--- day26/main.py
+++ day26/main.py
@@ -5,7 +5,16 @@ df = pd.read_csv("day26/nato_phonetic_alphabet.csv")
 
 new_dict_with_nato = {row.letter:row.code for (index, row) in df.iterrows()}
 
-user_input = input("enter a word: ")
+def generate_phonetic():
+    user_input = input("enter a word: ").upper()
 
-for letter in user_input:
-    print(f"{letter}: {new_dict_with_nato[letter.capitalize()]}")
+    try:
+    
+        output_list = [new_dict_with_nato[letter] for letter in user_input]
+    except KeyError:
+        print("please enter a word")
+        generate_phonetic()
+    else:
+        print(output_list)
+
+generate_phonetic()
